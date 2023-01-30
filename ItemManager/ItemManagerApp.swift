88e6c95203885d47347ab0cae206905f261894ae
Shion Koga <shion_koga@mail.toyota.co.jp>
2023-01-30 12:45:29 +0900

    Initial Commit

---
+++ ItemManager/ItemManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItemManagerApp.swift
+//  ItemManager
+//
+//  Created by 14inchの史苑 on 2023/01/30.
+//
+
+import SwiftUI
+
+@main
+struct ItemManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
